07c1b228784061c8718079a35381b3b08e4a8944
Alba <albadvg@yahoo.es>
2023-04-04 16:26:41 +0200

    responsiveness improvement and burguer menu behaviour

--- js/scripts.js
+++ js/scripts.js
@@ -12,7 +12,6 @@ for (i = 0 ; i<bars.length; i++){
     bars[i].style.transitionDelay = `${i / 100}s`;
     let halfHeight = parseFloat(barHeight)/2;
     animation2(bars[i], halfHeight);
-    console.log(halfHeight);
 }
 
 let bars2 = document.querySelectorAll('.bar2');
@@ -26,11 +25,32 @@ for (i = 0 ; i<bars2.length; i++){
     bars2[i].style.transitionDelay = `${i / 100}s`;
     let halfHeight = parseFloat(barHeight)/2;
     animation2(bars2[i], halfHeight);
-    console.log(halfHeight);
 }
 
+/////////////
 
+let burguerButton = document.querySelector('.hamburguer-icon');
+let closeButton = document.querySelector('.hamburguer-nav .bi-x-lg');
+let mobileMenu = burguerButton.nextElementSibling;
+let body =  document.querySelector('body');
 
+burguerButton.addEventListener('click', showMenu);
+closeButton.addEventListener('click', hideMenu)
 
+function showMenu() {
+    mobileMenu.classList.add('show-menu');
+    body.style.height = '100vh';
+    body.style.overflowY = 'hidden';
+}
+function hideMenu() {
+    mobileMenu.classList.remove('show-menu');
+    body.style.height = 'unset';
+    body.style.overflowY = 'hidden';
+}
+
+console.log(burguerButton)
+console.log(mobileMenu)
+console.log(closeButton)
+console.log(body)
 
 
