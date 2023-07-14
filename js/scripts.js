2dd779821cf696da29250ff12d45f9cb654d222d
Alba <albadvg@yahoo.es>
2023-07-14 02:16:50 +0200

    variacions

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,33 @@
-let bars = document.querySelectorAll('.bar');
+// let bars = document.querySelectorAll('.bar');
 
-function animation2(bar , height) {
-    bar.style.transform = `translateY(-${height}px)  rotate(-${i-10}deg) `;
-};
+// function animation2(bar , height) {
+//     bar.style.transform = `translateY(-${height}px)  rotate(-${i-10}deg) `;
+// };
 
-for (i = 0 ; i<bars.length; i++){
-    bars[i].style.height = `${i*(i/20)}px`;
-    let barHeight = bars[i].style.height;
-    bars[i].style.marginBottom = `-${barHeight}`;
-    bars[i].style.transition = `transform .5s ease`;
-    bars[i].style.transitionDelay = `${i / 100}s`;
-    let halfHeight = parseFloat(barHeight)/2;
-    animation2(bars[i], halfHeight);
-}
+// for (i = 0 ; i<bars.length; i++){
+//     bars[i].style.height = `${i*(i/20)}px`;
+//     let barHeight = bars[i].style.height;
+//     bars[i].style.marginBottom = `-${barHeight}`;
+//     bars[i].style.transition = `transform .5s ease`;
+//     bars[i].style.transitionDelay = `${i / 100}s`;
+//     let halfHeight = parseFloat(barHeight)/2;
+//     animation2(bars[i], halfHeight);
+// }
 
-let bars2 = document.querySelectorAll('.bar2');
+// let bars2 = document.querySelectorAll('.bar2');
 
 
-for (i = 0 ; i<bars2.length; i++){
-    bars2[i].style.height = `${i*(i/20)}px`;
-    let barHeight = bars2[i].style.height;
-    bars2[i].style.marginBottom = `-${barHeight}`;
-    bars2[i].style.transition = `transform .5s ease`;
-    bars2[i].style.transitionDelay = `${i / 100}s`;
-    let halfHeight = parseFloat(barHeight)/2;
-    animation2(bars2[i], halfHeight);
-}
+// for (i = 0 ; i<bars2.length; i++){
+//     bars2[i].style.height = `${i*(i/20)}px`;
+//     let barHeight = bars2[i].style.height;
+//     bars2[i].style.marginBottom = `-${barHeight}`;
+//     bars2[i].style.transition = `transform .5s ease`;
+//     bars2[i].style.transitionDelay = `${i / 100}s`;
+//     let halfHeight = parseFloat(barHeight)/2;
+//     animation2(bars2[i], halfHeight);
+// }
+//HERO BARS ANIMATION
+
 
 /////////////
 
@@ -48,18 +50,53 @@ function hideMenu() {
     body.style.overflowY = 'hidden';
 }
 
-console.log(burguerButton)
-console.log(mobileMenu)
-console.log(closeButton)
-console.log(body)
 
 
-///CAROUSEL
-$(".carousel-home").owlCarousel({
-    items:1,
-    nav:true,
-    merge:true,
-    dots: true,
-    lazyLoad:true,
-});
+//ABOUT US BLUE BARS
+let blueBars = document.querySelectorAll('.about-us-3 .about-underline');
 
+function blueUnderlineVisible(entries) {
+    entries.map((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.style.animation = "aboutUnderlineAnimation 2s ease";
+            entry.target.style.animationFillMode = "forwards";
+            observeBlueBars.unobserve(entry.target);
+        };
+    });
+};
+
+const observeBlueBars = new IntersectionObserver(blueUnderlineVisible);
+
+blueBars.forEach(bar => observeBlueBars.observe(bar));
+
+//LAZY LOAD
+
+let lazyElements = document.querySelectorAll('.lazy');
+
+function lazyElementVisible(entries) {
+    entries.map((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.style.opacity = "1";
+        }
+    });
+}
+
+const observeLazyImg = new IntersectionObserver(lazyElementVisible);
+
+lazyElements.forEach(lazyElement => observeLazyImg.observe(lazyElement));
+
+//SLIDE IN ELEMENTS
+
+let slideInElements = document.querySelectorAll('.spread-apart');
+
+function slideInVisible(entries) {
+    entries.map((entry) => {
+        if(entry.isIntersecting) {
+            entry.target.style.animation = "spreadAnimation 2s ease";
+            entry.target.style.animationFillMode = "forwards";
+        }
+    });
+};
+
+const slideInObserver = new IntersectionObserver(slideInVisible);
+slideInElements.forEach(element => slideInObserver.observe(element));
---
+++ owlcarousel/owl.setup.js
@@ -0,0 +1,16 @@
+///CAROUSEL
+
+$(".carousel-home").owlCarousel({
+    items:1,
+    nav:true,
+    merge:true,
+    dots: true,
+    lazyLoad:true,
+});
+$(".carousel-managers").owlCarousel({
+    items:1,
+    nav:true,
+    merge:true,
+    dots: true,
+    lazyLoad:true,
+});
