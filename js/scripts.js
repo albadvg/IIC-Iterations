6123a0c90584226b53ede132ebd648b0d2427484
Alba <albadvg@yahoo.es>
2023-07-16 23:44:41 +0200

    tempos animacion avatar

--- js/scripts.js
+++ js/scripts.js
@@ -73,35 +73,38 @@ function slideInVisible(entries) {
 const slideInObserver = new IntersectionObserver(slideInVisible);
 slideInElements.forEach(element => slideInObserver.observe(element));
 
-//TEAM AVATARS
-line1 = $('#line1');
-avatar1 = $('#avatar-1');
-avatar2 = $('#avatar-2');
+// //TEAM AVATARS
+// line1 = $('#line1');
+// avatar1 = $('#avatar-1');
+// avatar2 = $('#avatar-2');
 
-var position1 = avatar1.position();
-var position2 = avatar2.position();
+// var position1 = avatar1.position();
+// var position2 = avatar2.position();
 
-line1
-  .attr('x1', position1.left)
-  .attr('y1', position1.top)
-  .attr('x2', position2.left)
-  .attr('y2', position2.top);
+// line1
+//   .attr('x1', position1.left)
+//   .attr('y1', position1.top)
+//   .attr('x2', position2.left)
+//   .attr('y2', position2.top);
 
   //avatar changes
 
   let avatars = document.querySelectorAll('.avatar-circle');
+  let marginPercentage = (20 * window.innerHeight) / 100;   
+  console.log(marginPercentage);
 
   function isCenterViewport(element){
     const rect = element.getBoundingClientRect();
     return (
-        rect.top >= 200 &&
+        rect.top >= marginPercentage &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight -200 || document.documentElement.clientHeight - 200) &&
+        rect.bottom <= (window.innerHeight - marginPercentage * 1.5 || document.documentElement.clientHeight - marginPercentage * 1.5) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
   }
+console.log(isCenterViewport)
 
-  function avatarColor() {
+  function avatarPosition() {
     avatars.forEach(avatar => {
         if(isCenterViewport(avatar)){
             avatar.style.transform = 'scale(3) translate(20px)';
@@ -112,4 +115,5 @@ line1
     })
   }
   
-  window.addEventListener("scroll", avatarColor);
+  window.addEventListener("scroll", avatarPosition);
+
